580748528610e17a5b2624c811b4f28c948a4431
Michel <micaraujo71@gmail.com>
2023-10-30 21:33:36 -0300

    💄 Config base colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,23 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                // Base
+                'background': '#121214',
+                'shape-principal': '#202024',
+                'shape-secundaria': '#29292E',
+                'shape-terciaria': '#323238',
+                'placeholder': '#7C7C8A',
+                'text-base': '#E1E1E6',
+                'titles': '#E1E1E6',
+
+                // Produto
+                'green-dark': '#015F43',
+                'green': '#00875F',
+                'green-light': '#00B37E',
+                'red-dark': '#AA2834',
+                'red': '#F75A68'
+            }
         },
     },
 
