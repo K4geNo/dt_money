fe596f27758d08659bfac0baa3333cbcb1760ed8
KageNo <micaraujo71@gmail.com>
2023-11-17 20:36:07 -0300

    ✨ Add formattedPrice utility function

---
+++ resources/js/utils/fomartPrice.ts
@@ -0,0 +1,12 @@
+/**
+ * Formata um valor de preço em uma sequência de moeda.
+ *
+ * @param price - O valor do preço a ser formatado.
+ * @returns O preço formatado como uma string de moeda.
+ */
+export const formattedPrice = (price: number) => {
+    return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    }).format(price);
+}
