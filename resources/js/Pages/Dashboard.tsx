83d08f0b28ab4977a02b47da7d231f11cbdb24fa
KageNo <micaraujo71@gmail.com>
2023-11-17 20:35:04 -0300

    ♻️ Refactor Dashboard component

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,33 +1,49 @@
-import { useContext, useState } from 'react';
+import { useEffect, useState } from 'react'
 
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { CardTable } from '@/Components/CardTable';
-import { Head } from '@inertiajs/react';
-import { PageProps } from '@/types';
-import { Search } from '@/Components/Search';
-import { Summary } from '@/Components/Summary';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
+import { CardTable } from '@/Components/CardTable'
+import { Head } from '@inertiajs/react'
+import { PageProps } from '@/types'
+import { Search } from '@/Components/Search'
+import { Summary } from '@/Components/Summary'
+import { filterTransactions } from '@/utils/filterTransactions'
+import { sortTransactions } from '@/utils/sortTransactions'
+import { useCategories } from '@/hooks/useCategories'
 
-export default function Dashboard({ auth, transactions }: PageProps) {
+export default function Dashboard({
+    auth,
+    transactions,
+    categories,
+}: PageProps) {
     const [search, setSearch] = useState('')
 
-    const filteredTransactions = transactions.filter(e => e.description.includes(search))
+    const { setCategories } = useCategories()
+
+    useEffect(() => {
+        setCategories(categories)
+    }, [categories, setCategories])
+
+    const filteredTransactions = filterTransactions({
+        transactions,
+        categories,
+        search,
+    })
+
+    const sortedFilteredTransactions = sortTransactions(filteredTransactions)
 
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">Dashboard</h2>}
-        >
+        <AuthenticatedLayout user={auth.user}>
             <Head title="Dashboard" />
 
             <div className="py-12">
                 <div className="max-w-[1120px] mx-auto">
-                    <Summary />
+                    <Summary transactions={transactions} />
 
                     <Search setSearch={setSearch} />
 
                     <div className="mt-6 overflow-hidden bg-white shadow-sm dark:bg-shape-principal sm:rounded-lg">
                         <div className="flex flex-col text-gray-900 dark:text-gray-100 gap-y-2">
-                            {filteredTransactions.map((transaction) => (
+                            {sortedFilteredTransactions.map((transaction) => (
                                 <CardTable
                                     key={transaction.id}
                                     transaction={transaction}
@@ -38,5 +54,5 @@ export default function Dashboard({ auth, transactions }: PageProps) {
                 </div>
             </div>
         </AuthenticatedLayout>
-    );
+    )
 }
