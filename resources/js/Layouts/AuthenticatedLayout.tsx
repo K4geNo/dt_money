2d779f5184b5ed9ac86080bd23c1fdbe342ae529
KageNo <micaraujo71@gmail.com>
2023-11-17 20:34:42 -0300

    ♻️ Refactor GuestLayout component styles

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -1,23 +1,32 @@
-import { PropsWithChildren, ReactNode, useState } from 'react';
+import { PropsWithChildren, useState } from 'react'
 
-import Dropdown from '@/Components/Dropdown';
-import { Link } from '@inertiajs/react';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
-import { Separator } from '@/Components/Separator';
-import { TransactionModal } from '@/Components/TransactionModal';
-import { User } from '@/types';
+import Dropdown from '@/Components/Dropdown'
+import { Link } from '@inertiajs/react'
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink'
+import { Separator } from '@/Components/Separator'
+import { ToastContainer } from 'react-toastify'
+import { TransactionModal } from '@/Components/TransactionModal'
+import { User } from '@/types'
 
-export default function Authenticated({ user, header, children }: PropsWithChildren<{ user: User, header?: ReactNode }>) {
-    const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
+export default function Authenticated({
+    user,
+    children,
+}: PropsWithChildren<{
+    user: User
+}>) {
+    const [showingNavigationDropdown, setShowingNavigationDropdown] =
+        useState(false)
 
     return (
         <div className="w-full min-h-screen bg-gray-100 dark:bg-shape-principal">
-            <nav className="bg-white pt-10 dark:bg-background h-[212px] border-b border-background/10 dark:border-gray-700">
+            <nav className="bg-white pt-10 dark:bg-background h-[212px] border-background/10">
+                <ToastContainer />
+
                 <div className="max-w-[1120px] mx-auto">
                     <div className="flex justify-between h-16">
                         <div className="flex">
                             <div className="flex items-center shrink-0">
-                                <Link href="/" className='W-[172px]'>
+                                <Link href="/login" className="W-[172px]">
                                     <img src="/logo.svg" alt="" />
                                 </Link>
                             </div>
@@ -27,8 +36,8 @@ export default function Authenticated({ user, header, children }: PropsWithChild
                             <TransactionModal />
 
                             <Separator
-                                orientation='vertical'
-                                className='h-10 bg-white'
+                                orientation="vertical"
+                                className="h-10 bg-white"
                             />
 
                             <div className="relative">
@@ -37,7 +46,7 @@ export default function Authenticated({ user, header, children }: PropsWithChild
                                         <span className="inline-flex rounded-md">
                                             <button
                                                 type="button"
-                                                className="inline-flex items-center h-12 px-5 py-3 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md dark:text-gray-400 dark:bg-shape-terciaria hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none"
+                                                className="inline-flex items-center h-12 px-5 py-3 text-sm font-medium leading-4 transition duration-150 ease-in-out bg-white border border-transparent rounded-md text-text-base dark:text-text-base dark:bg-shape-terciaria hover:text-text-base dark:hover:text-text-base focus:outline-none"
                                             >
                                                 {user.name}
 
@@ -58,9 +67,20 @@ export default function Authenticated({ user, header, children }: PropsWithChild
                                     </Dropdown.Trigger>
 
                                     <Dropdown.Content>
-                                        <Dropdown.Link href={route('profile.edit')}>Profile</Dropdown.Link>
-                                        <Dropdown.Link href={route('logout')} method="post" as="button">
-                                            Log Out
+                                        <Dropdown.Link
+                                            href={route('profile.edit')}
+                                        >
+                                            Perfil
+                                        </Dropdown.Link>
+                                        <Dropdown.Link href={route('report')}>
+                                            Relátorios
+                                        </Dropdown.Link>
+                                        <Dropdown.Link
+                                            href={route('logout')}
+                                            method="post"
+                                            as="button"
+                                        >
+                                            Sair
                                         </Dropdown.Link>
                                     </Dropdown.Content>
                                 </Dropdown>
@@ -69,19 +89,36 @@ export default function Authenticated({ user, header, children }: PropsWithChild
 
                         <div className="flex items-center -mr-2 sm:hidden">
                             <button
-                                onClick={() => setShowingNavigationDropdown((previousState) => !previousState)}
+                                onClick={() =>
+                                    setShowingNavigationDropdown(
+                                        (previousState) => !previousState,
+                                    )
+                                }
                                 className="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400"
                             >
-                                <svg className="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                <svg
+                                    className="w-6 h-6"
+                                    stroke="currentColor"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                >
                                     <path
-                                        className={!showingNavigationDropdown ? 'inline-flex' : 'hidden'}
+                                        className={
+                                            !showingNavigationDropdown
+                                                ? 'inline-flex'
+                                                : 'hidden'
+                                        }
                                         strokeLinecap="round"
                                         strokeLinejoin="round"
                                         strokeWidth="2"
                                         d="M4 6h16M4 12h16M4 18h16"
                                     />
                                     <path
-                                        className={showingNavigationDropdown ? 'inline-flex' : 'hidden'}
+                                        className={
+                                            showingNavigationDropdown
+                                                ? 'inline-flex'
+                                                : 'hidden'
+                                        }
                                         strokeLinecap="round"
                                         strokeLinejoin="round"
                                         strokeWidth="2"
@@ -93,9 +130,17 @@ export default function Authenticated({ user, header, children }: PropsWithChild
                     </div>
                 </div>
 
-                <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
+                <div
+                    className={
+                        (showingNavigationDropdown ? 'block' : 'hidden') +
+                        ' sm:hidden'
+                    }
+                >
                     <div className="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
+                        <ResponsiveNavLink
+                            href={route('dashboard')}
+                            active={route().current('dashboard')}
+                        >
                             Dashboard
                         </ResponsiveNavLink>
                     </div>
@@ -105,12 +150,20 @@ export default function Authenticated({ user, header, children }: PropsWithChild
                             <div className="text-base font-medium text-gray-800 dark:text-gray-200">
                                 {user.name}
                             </div>
-                            <div className="text-sm font-medium text-gray-500">{user.email}</div>
+                            <div className="text-sm font-medium text-gray-500">
+                                {user.email}
+                            </div>
                         </div>
 
                         <div className="mt-3 space-y-1">
-                            <ResponsiveNavLink href={route('profile.edit')}>Profile</ResponsiveNavLink>
-                            <ResponsiveNavLink method="post" href={route('logout')} as="button">
+                            <ResponsiveNavLink href={route('profile.edit')}>
+                                Profile
+                            </ResponsiveNavLink>
+                            <ResponsiveNavLink
+                                method="post"
+                                href={route('logout')}
+                                as="button"
+                            >
                                 Log Out
                             </ResponsiveNavLink>
                         </div>
@@ -120,5 +173,5 @@ export default function Authenticated({ user, header, children }: PropsWithChild
 
             <main>{children}</main>
         </div>
-    );
+    )
 }
