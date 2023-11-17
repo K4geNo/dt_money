e6dd1911a62c57466d78c8c04210f44931afd1ec
KageNo <micaraujo71@gmail.com>
2023-11-17 20:35:42 -0300

    ♻️ Refactor types and add Categories interface

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,19 +1,31 @@
 export interface User {
-    id: number;
-    name: string;
-    email: string;
-    email_verified_at: string;
+    id: number
+    name: string
+    email: string
+    email_verified_at: string
 }
 
-export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
+export type PageProps<
+    T extends Record<string, unknown> = Record<string, unknown>,
+> = T & {
     auth: {
-        user: User;
-    };
+        user: User
+    }
     transactions: {
-        id: number;
-        description: string;
-        amount: number;
-        type: string;
+        id: number
+        description: string
+        amount: number
+        type: string
+        category_id: number
+        created_at: string
+    }[]
+    categories: {
+        id: number
         category: string
     }[]
-};
+}
+
+export interface Categories {
+    id: number
+    category: string
+}
