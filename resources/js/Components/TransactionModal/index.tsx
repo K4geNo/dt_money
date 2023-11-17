2c5f5a938c1b3152302c79e46e785f6a8f95dee1
KageNo <micaraujo71@gmail.com>
2023-11-17 20:33:13 -0300

    ♻️ Refactor TransactionModal component

--- resources/js/Components/TransactionModal/index.tsx
+++ resources/js/Components/TransactionModal/index.tsx
@@ -1,37 +1,71 @@
-import { ArrowDownCircle, ArrowUpCircle } from "lucide-react";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "../Dialog";
-import { RadioGroup, RadioGroupItem } from "../RadioGroup";
+import { ArrowDownCircle, ArrowUpCircle } from 'lucide-react'
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '../Dialog'
+import { RadioGroup, RadioGroupItem } from '../RadioGroup'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '../Select'
 
-import { Input } from "../Input";
-import { cn } from "@/lib/class-merge";
-import { useForm } from "@inertiajs/react";
-import { useState } from "react";
+import { Input } from '../Input'
+import { cn } from '@/lib/class-merge'
+import { toast } from 'react-toastify'
+import { useCategories } from '@/hooks/useCategories'
+import { useForm } from '@inertiajs/react'
+import { useState } from 'react'
 
 export function TransactionModal() {
-    const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false)
 
-    const { data, setData, post, errors, reset, processing } = useForm({
+    const { categories } = useCategories()
+
+    const { data, setData, post, reset, processing } = useForm({
         description: '',
         amount: '',
-        category: '',
+        category_id: 0,
         type: 'income',
     })
 
     const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+        e.preventDefault()
 
-        post(route('transactions.store'));
+        post(route('transactions.store'))
 
-        reset();
+        reset()
 
-        setOpen(false);
+        toast.success('Transação criada!', {
+            position: 'top-right',
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'dark',
+        })
+
+        setOpen(false)
+    }
+
+    const handleDescription = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const description = e.target.value
+
+        const uppercase =
+            description.charAt(0).toUpperCase() + description.slice(1)
+
+        setData('description', uppercase)
     }
 
     return (
-        <Dialog
-            open={open}
-            onOpenChange={setOpen}
-        >
+        <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>
                 <button className="bg-[#00875F] px-5 py-3 rounded-md text-white">
                     Nova transação
@@ -50,7 +84,7 @@ export function TransactionModal() {
                         <Input
                             placeholder="Descrição"
                             type="text"
-                            onChange={(e) => setData('description', e.target.value)}
+                            onChange={(e) => handleDescription(e)}
                             value={data.description}
                         />
                     </div>
@@ -65,26 +99,43 @@ export function TransactionModal() {
                     </div>
 
                     <div className="flex flex-col pt-4">
-                        <Input
-                            placeholder="Categoria"
-                            type="text"
-                            onChange={(e) => setData('category', e.target.value)}
-                            value={data.category}
-                        />
+                        <Select
+                            onValueChange={(value: string) => {
+                                setData('category_id', Number(value))
+                            }}
+                        >
+                            <SelectTrigger className="w-full h-[56px] border-none rounded-md bg-background text-text-base placeholder:text-placeholder focus:ring focus:ring-green-light">
+                                <SelectValue placeholder="Categorias" />
+                            </SelectTrigger>
+                            <SelectContent className="bg-background text-text-base">
+                                {categories.map((category) => (
+                                    <SelectItem
+                                        key={category.id}
+                                        textValue={category.category}
+                                        value={String(category.id)}
+                                        className="py-2 cursor-pointer hover:bg-shape-secundaria"
+                                    >
+                                        {category.category}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
                     </div>
 
                     <RadioGroup
                         defaultValue="income"
                         className="flex w-full pt-6 gap-x-4"
-                        onValueChange={(value: string) => setData('type', value)}
+                        onValueChange={(value: string) =>
+                            setData('type', value)
+                        }
                     >
                         <div className="flex items-center w-full">
                             <RadioGroupItem
                                 value="income"
                                 id="income"
                                 className={cn(
-                                    "w-full text-center border border-transparent rounded-md cursor-pointer h-14",
-                                    "bg-shape-secundaria radix-state-checked:bg-green-dark"
+                                    'w-full text-center border border-transparent rounded-md cursor-pointer h-14',
+                                    'bg-shape-secundaria radix-state-checked:bg-green-dark',
                                 )}
                             >
                                 <ArrowUpCircle className="inline w-6 h-6 mx-auto mr-2 text-white" />
@@ -96,8 +147,8 @@ export function TransactionModal() {
                                 value="expense"
                                 id="expense"
                                 className={cn(
-                                    "w-full text-center border border-transparent rounded-md cursor-pointer h-14",
-                                    "bg-shape-secundaria radix-state-checked:bg-red-dark"
+                                    'w-full text-center border border-transparent rounded-md cursor-pointer h-14',
+                                    'bg-shape-secundaria radix-state-checked:bg-red-dark',
                                 )}
                             >
                                 <ArrowDownCircle className="inline w-6 h-6 mx-auto mr-2 text-white" />
