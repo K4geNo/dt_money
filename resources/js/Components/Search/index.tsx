3111495e8a0f81caa890e41453353ca8d8d6a6a8
KageNo <micaraujo71@gmail.com>
2023-11-17 20:32:19 -0300

    🎨 Add new dependencies and fix UI issues

--- resources/js/Components/Search/index.tsx
+++ resources/js/Components/Search/index.tsx
@@ -1,4 +1,4 @@
-import { Input } from "../Input";
+import { Input } from '../Input'
 
 interface SearchProps {
     setSearch: (search: string) => void
@@ -13,20 +13,11 @@ export function Search({ setSearch }: SearchProps) {
 
     return (
         <div className="flex w-full mt-16 gap-x-4">
-            <Input className="flex-1 w-full focus:ring focus:ring-green-light"
+            <Input
+                className="flex-1 w-full border-none focus:ring focus:ring-green-light"
                 placeholder="Busque por uma transação"
-                onChange={e => handleSearch(e)}
+                onChange={(e) => handleSearch(e)}
             />
-
-            <button
-                className="py-[14px] px-8 border border-green-light rounded-md text-green-light"
-            >
-                Buscar
-            </button>
         </div>
     )
 }
-
-
-
-
