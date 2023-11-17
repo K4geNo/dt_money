e585d1a9c099aa56d869e64c3dfbbdebe2030cbc
KageNo <micaraujo71@gmail.com>
2023-11-17 20:36:29 -0300

    🐛 Fix import statements and add CategoryProvider

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,26 +1,30 @@
-import './bootstrap';
-import '../css/app.css';
+import './bootstrap'
+import '../css/app.css'
 
-import { TransactionProvider } from './Context/TransactionContext';
-import { createInertiaApp } from '@inertiajs/react';
-import { createRoot } from 'react-dom/client';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { CategoryProvider } from './Context/CategoryContext'
+import { createInertiaApp } from '@inertiajs/react'
+import { createRoot } from 'react-dom/client'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.tsx`,
+            import.meta.glob('./Pages/**/*.tsx'),
+        ),
     setup({ el, App, props }) {
-        const root = createRoot(el);
+        const root = createRoot(el)
 
         root.render(
-            <TransactionProvider>
+            <CategoryProvider>
                 <App {...props} />
-            </TransactionProvider>
-        );
+            </CategoryProvider>,
+        )
     },
     progress: {
         color: '#4B5563',
     },
-});
+})
