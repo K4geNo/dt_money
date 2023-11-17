f602921de9acc70a75d73a7cc013d2bb3078f7f0
KageNo <micaraujo71@gmail.com>
2023-11-17 20:31:33 -0300

    ♻️ Refactor Input component styles

--- resources/js/Components/Input/index.tsx
+++ resources/js/Components/Input/index.tsx
@@ -2,8 +2,7 @@ import * as React from 'react'
 
 import { cn } from '@/lib/class-merge'
 
-export interface InputProps
-    extends React.InputHTMLAttributes<HTMLInputElement> {}
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
     ({ className, type, ...props }, ref) => {
@@ -11,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             <input
                 type={type}
                 className={cn(
-                    'p-4 rounded-md bg-background text-placeholder',
+                    'p-4 rounded-md bg-background text-text-base border-none placeholder:text-placeholder focus:ring focus:ring-green-light',
                     className,
                 )}
                 ref={ref}
